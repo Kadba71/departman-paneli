8506560af5a92fb7200f4647dadb30d86985d420
Kadba71 <mertcann151515@gmail.com>
2025-07-23 20:18:28 +0300

    Trigger deploy for migration

--- departman_paneli/settings.py
+++ departman_paneli/settings.py
@@ -85,4 +85,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL
+# ...existing code...
+LOGIN_URL = '/accounts/login/'  # migration tetiklemek için değişiklik
+# ...existing code...
